a754669384bbfe634fc97f7dd9b6ba9f597a24d0
solange nieto <rocionie99@gmail.com>
2023-08-24 23:12:24 -0400

    arreglos

--- listacontacto.js
+++ listacontacto.js
@@ -1,44 +1,52 @@
-let listacontactos = {
+let listacontactos = [{
     id : 1,
     nombre: "solange",
     apellido: "nieto",
     telefono: 322000,
     ubicacion: { ciudad : "cucuta", direccion: "calle 3 #577888",}
 
-}
+},
 
-
-    id : 2,{
+{
+    id : 2,
     nombre: "romel",
     apellido: "nieto",
     telefono: 2566600,
     ubicacion: { ciudad : "medellin", direccion: "calle 26 #5888",}
 
-    }
-
-    id : 3,{
-        nombre: "otto",
-        apellido: "nieto",
-        telefono: 8882222,
-        ubicacion: { ciudad : "bucaramanga", direccion: "calle 2 #555 ",
-    }
-      }
+    },
+];
 
 function agregarcontacto ( nuevocontacto){
     listacontactos.push(nuevocontacto)
 
 }
 function borracontacto(conctatoaborrar){
-    let nuevalista = listacontactos.filter((contacto)=>contacto !== conctatoaborrar);
+    let nuevalista = listacontactos.filter((contacto) => contacto.id !== conctatoaborrar);
 listacontactos = nuevalista
 }
+
+
 
 function imprimircontacto(){
     console.log(listacontactos)
 }
+let nuevocontacto= 
+{
+   id: 3,
+       nombre: "otto",
+       apellido: "nieto",
+       
+       telefono: 8882222,
+       ubicacion: { ciudad : "bucaramanga", direccion: "calle 2 #555 ",
+    }
+ }
 
-borracontacto("RomelNieto")
-agregarcontacto("AndresNieto");
-imprimircontacto ()
-agregarcontacto("JordanCarrillo");
-console.log(listacontactos)
+
+
+agregarcontacto(nuevocontacto);
+console.log(nuevocontacto);
+borracontacto(2);
+imprimircontacto ();
+//agregarcontacto("");
+//console.log(listacontactos);
