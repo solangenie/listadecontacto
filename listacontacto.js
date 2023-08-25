8a1efbbc40194cbbf5e4fca3feb3665dbc8c49f0
solange nieto <rocionie99@gmail.com>
2023-08-24 22:00:24 -0400

    proyecto

--- listacontacto.js
+++ listacontacto.js
@@ -1,9 +1,32 @@
-let listacontactos = ["FannyGomez", "RomelNieto", "OttoNieto",]
+let listacontactos = {
+    id : 1,
+    nombre: "solange",
+    apellido: "nieto",
+    telefono: 322000,
+    ubicacion: { ciudad : "cucuta", direccion: "calle 3 #577888",}
 
+}
+
+
+    id : 2,{
+    nombre: "romel",
+    apellido: "nieto",
+    telefono: 2566600,
+    ubicacion: { ciudad : "medellin", direccion: "calle 26 #5888",}
+
+    }
+
+    id : 3,{
+        nombre: "otto",
+        apellido: "nieto",
+        telefono: 8882222,
+        ubicacion: { ciudad : "bucaramanga", direccion: "calle 2 #555 ",
+    }
+      }
 
 function agregarcontacto ( nuevocontacto){
     listacontactos.push(nuevocontacto)
-return listacontactos
+
 }
 function borracontacto(conctatoaborrar){
     let nuevalista = listacontactos.filter((contacto)=>contacto !== conctatoaborrar);
